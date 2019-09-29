24805be660ec2c27b8d7912b1c254841f104e8c4
Den <derofim@yandex.ru>
2019-09-29 21:33:29 +0300

    better deps management

--- scripts/install_cmake.sh
+++ scripts/install_cmake.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+﻿#!/bin/bash
 # Copyright (c) 2019 Denis Trofimov (den.a.trofimov@yandex.ru)
 # Distributed under the MIT License.
 # See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
