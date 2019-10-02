667226f7b524ee3f91ce7405bf0b9b9f1069a385
trofimov_d_a <trofimov_d_a@magnit.ru>
2019-10-02 08:34:59 +0300

    no `sudo` in scripts

--- scripts/install_cmake.sh
+++ scripts/install_cmake.sh
@@ -15,12 +15,12 @@ pushd ~/temp
 
 wget https://github.com/Kitware/CMake/releases/download/v$version/cmake-$version-Linux-x86_64.sh
 
-sudo rm -rf /opt/cmake || true
-sudo mkdir /opt/cmake
+rm -rf /opt/cmake || true
+mkdir /opt/cmake
 
-sudo sh cmake-$version-Linux-x86_64.sh --prefix=/opt/cmake --skip-license
+sh cmake-$version-Linux-x86_64.sh --prefix=/opt/cmake --skip-license
 
-sudo rm /usr/local/bin/cmake || true
-sudo ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
+rm /usr/local/bin/cmake || true
+ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
 
 cmake --version
